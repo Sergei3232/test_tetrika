b3b98e0aa645afb84c772712a33b0ee645eef61e
Sergei3232 <45172095+Sergei3232@users.noreply.github.com>
2019-09-28 12:20:59 +0300

    Correction project1

--- src/component/Test1/Test1.js
+++ src/component/Test1/Test1.js
@@ -2,47 +2,32 @@ import React from 'react';
 
 function SearchPairs(){
     
-    const arrSort = [1, 2, 6, 5, 3, 4, 7, 8];
+    const arrSort = [1, 2, 6, 5, 3, 4, 7, 8, 2, 3];
     const intSort = 5;
     const arrReturn = [];
 
     for (let i = 0; i < arrSort.length; i++){
         let nambPr = arrSort[i]; 
         
-        if (nambPr>=intSort){
+        let namberRz = intSort - nambPr;
+        
+        if(namberRz <= 0){
             continue;
         }
-        else{
 
-            for (let j = (i+1); j <= arrSort.length-1; j++){
+        if (arrSort.indexOf(namberRz) != -1){           
                 
-                let sumNamb = (nambPr + arrSort[j]);
-                
-                if (sumNamb==intSort) {
-                    
-                    let valueZn = (nambPr > arrSort[j])? `${String(arrSort[j])},${String(nambPr)}` : `${String(nambPr)},${String(arrSort[j])}`; 
-                    // console.log(valueZn);
+                let valueZn = (nambPr > namberRz)? `${String(namberRz)},${String(nambPr)}` : `${String(nambPr)},${String(namberRz)}`; 
 
-                    if (arrReturn.indexOf(valueZn) == -1){
-                        arrReturn.push(valueZn);
-                    }
-                    // if (nambPr > arrSort[j]){                            
-                    //     let valueZn = arrReturn.push(`${String(arrSort[j])},${String(nambPr)}`);
-                    // }
-                    // else{
-                    //     arrReturn.push(`${String(nambPr)},${String(arrSort[j])}`); 
-                    // }
-                                
-                }
-                // Проверяем уникальность записи
-                
-            }
+                if (arrReturn.indexOf(valueZn) == -1){
+                    arrReturn.push(valueZn);
+                }                                      
         }
 
     }
     return (
         <div>
-            <h1>{arrReturn}</h1>
+            <h1>{arrReturn.join("|")}</h1>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,3 @@ import ReactDOM from 'react-dom';
 import SearchPairs from './component/Test1/Test1';
 
 ReactDOM.render(<SearchPairs/>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
